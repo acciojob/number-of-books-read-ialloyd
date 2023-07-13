90e066f0ab3ce6962b9116af52f882bbf91ac86a
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-13 11:00:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,8 +16,17 @@ const library = [
   },
 ];
 
-const numberOfBooksRead = () => {
+const numberOfBooksRead = (library) => {
   // write your code here
+	let count=0;
+	library.forEach((element)=>{
+		if(element.readingStatus==true){
+
+			++count;
+		}
+	})
+
+	return count;
 };
 
 // Do not change the code below
